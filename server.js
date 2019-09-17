7868cf592f8777058403b29b1b209c0592d6bf07
HongliangWang.haining <whl7770@gmail.com>
2019-09-17 15:59:12 +0800

    add prduct

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     mongoose = require('mongoose'),
-    mongoConfig = require('./mongoDB');
+    mongoConfig = require('./mongoDB'),
+    productRoute = require('./app/product.dao');
 
     mongoose.Promise = global.Promise;
     mongoose.connect(mongoConfig.DB, { useNewUrlParser: true }).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
+      () => {console.log('BanWaGong Mango database is connected!') },
+      err => { console.log('Can not connect to the BanWaGong Mango database!'+ err)}
     );
 
     const app = express();
     app.use(bodyParser.json());
     app.use(cors());
+    app.use('/products', productRoute);
     const port = process.env.PORT || 4000;
 
     const server = app.listen(port, function(){
-     console.log('Listening on port ' + port);
+     console.log('Listening on port ' + port + "!");
     });
