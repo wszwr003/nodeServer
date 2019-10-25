d3d006ac184e665569e1602aed5caba486b43b08
HongliangWang.mac <whl7770@gmail.com>
2019-10-25 15:20:45 +0800

    Dock set

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:alpine
+
+# COPY nginx.conf /etc/nginx/nginx.conf
+
+# WORKDIR /usr/share/nginx/html
+# COPY dist/angularWeb .
--- app/product.mysql.dao.js
+++ app/product.mysql.dao.js
@@ -29,6 +29,7 @@ productRoutes.route('/add').post(function (req, res) {
         //res.status(400).send("unable to update the database");
     });
 });
+//GET PRODUCT LIST
 productRoutes.route('/list').get(function (req, res) {
     mysqlConnect.getConnection(function(error, connection){
         if (error) throw err;
@@ -42,12 +43,62 @@ productRoutes.route('/list').get(function (req, res) {
                 }
                 else
                     throw error; 
-            }
+            } 
             console.log('4.MySQL Results: ',result); 
             res.json(result);
             connection.release();//TOSOLVE: 连接池需不需要release
             console.log("5.MySQL Release!");
         });
+        //res.status(400).send("unable to update the database");
+    });
+});
+//DELETE PRODUCT
+productRoutes.route('/delete').post(function (req, res) {
+    console.log('1.ProductName: DELETE FROM `product_inf` WHERE product_name = \''+req.body.ProductName+'\';');
+    mysqlConnect.getConnection(function(error, connection){
+        if (error) throw err;
+        console.log("2.MySQL Connected!");
+        var string = 'DELETE FROM `product_inf` WHERE product_name = \''+req.body.ProductName+'\';';
+        console.log('3.MySQL Instructions: ', string); 
+        connection.query(string, function (error, result, fields) { 
+            if (error){
+                if(error.code=='ER_DUP_ENTRY'){//判断是不是重复命名project
+                    console.log('ERR.ER_DUP_ENTRY'); 
+                }
+                else
+                    throw error; 
+            }
+            console.log('4.MySQL Results: ', result); 
+            connection.release();//TOSOLVE: 连接池需不需要release
+            console.log("5.MySQL Release!");
+
+        });
+        res.json('Update complete');
+        //res.status(400).send("unable to update the database");
+    });
+});
+//UPDATE PRODUCT
+productRoutes.route('/update').post(function (req, res) {
+    console.log('1.ProductName: DELETE FROM `product_inf` WHERE product_name = \''+req.body.ProductName+'\';');
+    mysqlConnect.getConnection(function(error, connection){
+        if (error) throw err;
+        console.log("2.MySQL Connected!");
+        var string = 'UPDATE `product_inf` SET WHERE product_name =';
+        console.log('3.MySQL Instructions: ', string); 
+        connection.query(string, function (error, result, fields) { 
+            if (error){
+                if(error.code=='ER_DUP_ENTRY'){//判断是不是重复命名project
+                    console.log('ERR.ER_DUP_ENTRY'); 
+                }
+                else
+                    throw error; 
+            }
+            console.log('4.MySQL Results: ', result); 
+            connection.release();//TOSOLVE: 连接池需不需要release
+            console.log("5.MySQL Release!");
+
+        });
+        res.json('Update complete');
         //res.status(400).send("unable to update the database");
     });
 });
